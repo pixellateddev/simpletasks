04198c54f118cfcaa45fcaca13e2df65cfaa9d7d
Himanshu Sagar <h_sagar@apple.com>
2025-11-02 13:19:49 +0530

    added mantine

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
